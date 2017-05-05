c37699d6fce153da3bb8f3e14359d44faaf2314c
Katie <raokatie324@gmail.com>
2017-05-05 14:04:04 -0400

    Add html case

--- MicroserviceBootdemo/src/main/java/demo/webController.java
+++ MicroserviceBootdemo/src/main/java/demo/webController.java
@@ -1,14 +1,17 @@
 package demo;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class webController {
 	
-	@RequestMapping("/home")
-	public @ResponseBody String message() {
-		return "Success!";
+	@RequestMapping("/hi/{name}")
+	public String message(Map model, @PathVariable String name) {
+		model.put("name", name);
+		return "home";
 	}
 }
